df302c41c81dce0d43d194664a4b128d1e57fb27
MtG <119689912+mpgzz@users.noreply.github.com>
2025-05-26 15:57:16 -0600

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+  },
+  ignorePatterns: [
+    "front/",
+    "node_modules/",
+    ".git/",
+    "dist/",
+    "build/",
+    "coverage/"
+  ]
+};
