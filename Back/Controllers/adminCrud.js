00f36266932db3dd5b21e090ec3487891944f464
MtG <119689912+mpgzz@users.noreply.github.com>
2025-05-26 17:19:20 -0600

    Update adminCrud.js

--- Back/Controllers/adminCrud.js
+++ Back/Controllers/adminCrud.js
@@ -4,9 +4,9 @@ const crypto = require('crypto');
 
 const pool = new sql.ConnectionPool({
   user: process.env.DB_USER,
-  password: process.env.PW,
-  server: process.env.SERVER,
-  database: process.env.DB,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
   port: parseInt(process.env.PORT, 10)
 });
 
