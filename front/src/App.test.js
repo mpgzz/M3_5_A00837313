4c94161165c3f9f9323f027564b552ca6686fafb
MtG <119689912+mpgzz@users.noreply.github.com>
2025-05-26 15:44:45 -0600

    Update App.test.js

--- front/src/App.test.js
+++ front/src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the login page correctly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const signInHeading = screen.getByRole('heading', { name: /sign in/i });
+  expect(signInHeading).toBeInTheDocument();
+
+  const emailInput = screen.getByLabelText(/email/i);
+  expect(emailInput).toBeInTheDocument();
+
+  const passwordInput = screen.getByLabelText(/password/i);
+  expect(passwordInput).toBeInTheDocument();
+
+  const signInButton = screen.getByRole('button', { name: /sign in with email and password/i });
+  expect(signInButton).toBeInTheDocument();
 });
